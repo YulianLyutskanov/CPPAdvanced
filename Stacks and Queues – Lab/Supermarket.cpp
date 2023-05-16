eebc027f6358b3500789af7a6d090fd0e8226e8a
YulianLyutskanov <126861539+YulianLyutskanov@users.noreply.github.com>
2023-05-16 17:06:13 +0300

    Add files via upload

---
+++ Stacks and Queues – Lab/Supermarket.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <queue>
+#include <string>
+
+using namespace std;
+int main()
+{
+    queue<string> clients;
+    string input;
+    int count = 0;\
+        cin >> input;
+    while (input != "End") {
+        
+        if (input == "Paid") {
+            while (clients.size()) {
+                cout << clients.front()<<endl;
+                clients.pop();
+                count--;
+            }
+        }
+        else {
+            clients.push(input);
+            count++;
+        }
+        cin >> input;
+    }
+    cout << count << " people remaining.";
+    
+}
